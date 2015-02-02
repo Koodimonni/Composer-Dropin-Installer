65bb6be12cf981ffc3d7812329d6dc0f6d00dd0f
Onni Hakala <onni@koodimonni.fi>
2015-02-02 21:00:32 +0200

    added travis.yml and basic file test with wordpress finnish language

---
+++ tests/FilesExistTest.php
@@ -0,0 +1,14 @@
+<?php
+class FileExistsTest extends PHPUnit_Framework_TestCase
+{
+
+  public function testFinnishLanguageWasInstalled()
+  {
+      $this->assertFileExists(dirname( __FILE__ ) . '/htdocs/wp-content/languages/fi.po');
+  }
+  public function testFinnishLanguageNotExistsInVendor()
+  {
+      $this->assertFileNotExists(dirname( __FILE__ ) . '/vendor/koodimonni-language/fi/fi.po');
+  }
+}
+?>
